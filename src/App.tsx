8442962c52b53c03a589dd58ad91c6bef320a4b2
jacky <jacky123789456@gmail.com>
2023-04-21 15:04:46 +0800

    top header 大致ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import logo from './logo.svg'
-import { Layout, Typography, Input } from 'antd';
+import type { MenuProps } from 'antd';
+import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd';
+import { GlobalOutlined } from '@ant-design/icons'
 
-function App() {
+const App = () => {
+  const items: MenuProps['items'] = [
+    { key: '1', label: '中文' },
+    { key: '2', label: 'English'}
+  ]
+
   return (
     <div className="App">
       <div className="app-header">
+        {/* top-header */}
+        <div className="container">
+          <div className="top-header">
+            <Dropdown.Button
+              style={{ marginTop: 5 }}
+              menu={{
+                items
+              }}
+              icon={ <GlobalOutlined /> }
+            >
+              語言
+            </Dropdown.Button>
+            <Button.Group className="button-group">
+              <Button>註冊</Button>
+              <Button>登入</Button>
+            </Button.Group>
+          </div>
+        </div>
+
         <Layout.Header className="main-header">
           <img src={logo} alt="" className="App-logo" />
           <Typography.Title level={3} className="title">React 旅遊網</Typography.Title>
