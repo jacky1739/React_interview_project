99a86c50499ff69db45944a976c0712b9a005894
jacky <jacky123789456@gmail.com>
2023-04-21 11:04:09 +0800

    header 完成

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { Layout, Typography, Input } from 'antd';
 function App() {
   return (
     <div className="App">
-      <div>
-        <Layout.Header>
+      <div className="app-header">
+        <Layout.Header className="main-header">
           <img src={logo} alt="" className="App-logo" />
-          <Typography.Title level={3}>React 旅遊網</Typography.Title>
-          <Input.Search placeholder="請輸入旅遊目的地或關鍵字" />
+          <Typography.Title level={3} className="title">React 旅遊網</Typography.Title>
+          <Input.Search placeholder="請輸入旅遊目的地或關鍵字" className="search-input" />
         </Layout.Header>
       </div>
     </div>
