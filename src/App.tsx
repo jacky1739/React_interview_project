85bc505e744ddf472d5d515094882e488b98de7e
jacky <jacky123789456@gmail.com>
2023-04-24 20:42:57 +0800

    scss 新增智能提示

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './App.module.scss'
 import { Header, Footer } from './component'
+import { Row, Col } from 'antd'
 
 const App: React.FC = () => {
 
   return (
     <div className={styles.App}>
       <Header />
+      {/* 頁面內容content */}
+      <div className={styles['page-content']}>
+        <Row style={{ margin: 20 }}>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </div>
       <Footer />
     </div>
   )
