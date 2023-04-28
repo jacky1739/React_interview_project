9797f83e5bf11584faa7af52f565ee991c898f64
jacky <jacky123789456@gmail.com>
2023-04-28 18:13:22 +0800

    更改成HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './App.module.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { HomePage, SignInPage, RegisterPage, DetailPage } from './pages'
 
 const App: React.FC = () => {
 
   return (
     <div className={styles.App}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signin" element={<SignInPage />} />
@@ -15,7 +15,7 @@ const App: React.FC = () => {
           <Route path="/detail/:touristRouteId" element={<DetailPage />} />
           <Route path="*" element={<h1>你的頁面跑去火星了</h1>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
